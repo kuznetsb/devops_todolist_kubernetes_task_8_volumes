9bad0acb9c2c10bfbfe77ac45b7257c131ed0ee4
kuznetsb <kuznetsbog@gmail.com>
2025-05-02 11:33:44 +0300

    Solution

---
+++ INSTRUCTION.md
@@ -0,0 +1,6 @@
+1. App is available by this URL `http://127.0.0.1:30007/`
+```bash
+kubectl exec <pod_id> -it -- sh
+```
+2. cd configs and ls
+3. cd secrets and ls
--- bootstrap.sh
+++ bootstrap.sh
@@ -1,1 +1,11 @@
 #!/bin/bash
+kind create cluster --config cluster.yml \
+&& cd .infrastructure \
+&& kubectl apply -f namespace.yml \
+&& kubectl apply -f pv.yml -n todoapp \
+&& kubectl apply -f pvc.yml -n todoapp \
+&& kubectl apply -f clusterIp.yml -n todoapp \
+&& kubectl apply -f nodePort.yml -n todoapp \
+&& kubectl apply -f secret.yml -n todoapp \
+&& kubectl apply -f confgiMap.yml -n todoapp \
+&& kubectl apply -f deployment.yml -n todoapp
